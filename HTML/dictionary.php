f35c1d20696841060fb0b4e60c302323f3f370c5
baxxem <basem.341@gmail.com>
2020-11-14 08:05:12 +0200

    clean up

---
+++ HTML/dictionary.php
@@ -0,0 +1,46 @@
+<?php 
+
+$dictionary = [
+
+	"EN" [
+		//Config.
+		"name": "English";
+		"direction": "ltr";
+		"align": "left;"
+
+		//Expression
+		"home" => "Home",
+		"about" => "About",
+		"attractions" => "Attractions",
+		"contact" => "Contact",
+		"dodgeball-signle" => "Dodgeball",
+		"donut-single" => "Donut",
+		"events" => "Events",
+		"freejump-single" => "Free Jump",
+		"ninjachallenge-single" => "Ninja Challenge",
+		"performance-single" => "Performance Challenge",
+		"programs" => "Programs",
+		"spiderwall-signle" => "Spiderwall"
+		"wipeout-signle" => "Wipeout "
+
+
+
+	],
+
+	"AR" [
+
+		//Config.
+		"name": "عربي";
+		"direction": "rtl";
+		"align": "right;
+
+
+
+
+	],
+
+];
+
+$default_language ="EN";
+
+?>
